d87f230016521437955588fcbff79166abceb935
Rayerleier <1045643889@qq.com>
2024-04-23 14:21:01 +0800

    first revise

--- TokenBank.sol
+++ TokenBank.sol
@@ -1,67 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IERC20 {
-    function balanceOf(address _owner) external  view returns (uint256 balance);
-    function transfer(address _to, uint256 _value) external  returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) external  payable returns (bool success);
-    function approve(address _spender, uint256 _value) external  returns (bool success);
-    function allowance(address _owner, address _spender) external  view returns (uint256 remaining);
-}
+// Write a TokenBank contract that can deposit your own Token into TokenBank and withdraw from TokenBank.
 
+// TokenBank has two methods:
 
-abstract contract BaseERC20 is IERC20  {
-    string public name; 
-    string public symbol; 
-    uint8 public decimals; 
+// deposit(): needs to record the deposit amount for each address;
+// withdraw(): users can withdraw their previously deposited tokens.
+// Enter your code or github link in the answer box.
 
-    uint256 public totalSupply; 
+contract TokenBank{
+    mapping (address => mapping (address=>uint256)) balances;
 
-    mapping (address => uint256) balances; 
+    event Deposited(address indexed user, uint256 amount);
 
-    mapping (address => mapping (address => uint256)) allowances; 
+    function deposit(address _constractAdress, uint256 _amount)public returns (bool){
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+        // 确认ERC20中存在balance
+        (bool success, bytes memory data) = _constractAdress.call(
+            abi.encodeWithSignature("balanceOf(address _owner)", msg.sender)
+        );
+        uint256 result = abi.decode(data, (uint256));
+        require(result>=_amount, "Not enough balance in ERC20.");
+        require(success, "Request failed.");
 
-    constructor()  {
-        name = "BaseERC20";
-        symbol = "BERC20";
-        decimals = 18;
-        totalSupply = 100000000 * 10 ** uint256(decimals); 
-        balances[msg.sender] = totalSupply;
+        // 确认ERC20中存在allowance
+        (bool allowanceSuccess, bytes memory allowanceData) = _constractAdress.call(
+            abi.encodeWithSignature("allowance(address _owner, address _spender)", msg.sender,this)
+        );
+        uint256 allowanceResult = abi.decode(allowanceData, (uint256));
+        require(allowanceResult>=_amount,"Not enough allowance in ERC20.");
+        require(allowanceSuccess, "Allowance Request failed.");
 
+        // 从ERC20中转账
+        (bool transferSuccess,) = _constractAdress.call(
+            abi.encodeWithSignature("transferFrom(address _from, address _to, uint256 _value)", msg.sender, this, _amount)
+        );
+        require(transferSuccess, "Transfer Failed.");
+        balances[_constractAdress][msg.sender] += _amount;
+        emit Deposited(msg.sender, _amount);
+        return transferSuccess;
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
+
+    function withdraw(address _constractAdress, uint256 _amount)public returns (bool){
+        require(balances[_constractAdress][msg.sender]>=_amount, "Not enough balances in TokenBank");
+        (bool withdrawSuccess, ) = _constractAdress.call(
+            abi.encodeWithSignature("transfer(address _to, uint256 _value)", _constractAdress, _amount)
+        );
+        require(withdrawSuccess, "Withdraw Failed.");
+        balances[_constractAdress][msg.sender] -= _amount;
+        return withdrawSuccess;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender]>= _value, "ERC20: transfer amount exceeds balance");
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);  
-        return true;   
-    }
 
-    function transferFrom(address _from, address _to, uint256 _value) public payable returns (bool success) {
-        require(allowances[_from][msg.sender] >= _value, "ERC20: transfer amount exceeds allowance");
-        require(balances[_from]>= _value, "ERC20: transfer amount exceeds balance");
-        balances[_from] -= _value;
-        balances[_to] += _value;
-        allowances[_from][msg.sender] -= _value;
-        emit Transfer(_from, _to, _value); 
-        return true; 
-    }
+    
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowances[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value); 
-        return true; 
-    }
-
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {  
-        return allowances[_owner][_spender];
-    }
 }
